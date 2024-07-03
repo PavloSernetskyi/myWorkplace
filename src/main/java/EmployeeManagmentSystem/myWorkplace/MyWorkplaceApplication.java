0bd3980a0d5499a74a1af641a3d6844d16276411
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2024-07-03 13:00:38 -0700

    initialized spring boot project with dependencies for rest api, mysql

---
+++ src/main/java/EmployeeManagmentSystem/myWorkplace/MyWorkplaceApplication.java
@@ -0,0 +1,13 @@
+package EmployeeManagmentSystem.myWorkplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EmployeeManagmentSystem/myWorkplace/MyWorkplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeeManagmentSystem.myWorkplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
